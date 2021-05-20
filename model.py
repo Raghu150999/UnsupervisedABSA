2f70a6a222e86fbacc6b7bfc465815e1f26bd59c
Raghu <raghu15sep99@gmail.com>
2021-05-20 12:12:02 +0530

    Added LQ loss

--- model.py
+++ model.py
@@ -1,7 +1,26 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
 from transformers import BertModel
 
+class LQLoss(nn.Module):
+    
+    def __init__(self, q, weight, alpha=0.0):
+        super().__init__()
+        self.q = q ## parameter in the paper
+        self.alpha = alpha ## hyper-parameter for trade-off between weighted and unweighted GCE Loss
+        self.weight = nn.Parameter(F.softmax(torch.log(1 / torch.tensor(weight)), dim=-1), requires_grad=False).to('cuda') ## per-class weights
+
+    def forward(self, input, target, *args, **kwargs):
+        bsz, _ = input.size()
+
+        Yq = torch.gather(input, 1, target.unsqueeze(1))
+        lq = (1 - torch.pow(Yq, self.q)) / self.q
+
+        _weight = self.weight.repeat(bsz).view(bsz, -1)
+        _weight = torch.gather(_weight, 1, target.unsqueeze(1))
+    
+        return torch.mean(self.alpha * lq + (1 - self.alpha) * lq * _weight)
 
 class BERTLinear(nn.Module):
     def __init__(self, bert_type, num_cat, num_pol):
@@ -10,6 +29,8 @@ class BERTLinear(nn.Module):
             bert_type, output_hidden_states=True)
         self.ff_cat = nn.Linear(768, num_cat)
         self.ff_pol = nn.Linear(768, num_pol)
+        self.aspect_weights = [345, 67, 201] # aspect category distribution (restaurant)
+        self.sentiment_weights = [231, 382] # sentiment category distribution (restaurant)
 
     def forward(self, labels_cat, labels_pol, **kwargs):
         outputs = self.bert(**kwargs)
@@ -22,6 +43,5 @@ class BERTLinear(nn.Module):
 
         logits_cat = self.ff_cat(se)  # (bsz, num_cat)
         logits_pol = self.ff_pol(se)  # (bsz, num_pol)
-        loss = nn.MultiMarginLoss()(logits_cat, labels_cat) + \
-            nn.MultiMarginLoss()(logits_pol, labels_pol)
+        loss = LQLoss(0.4, self.aspect_weights)(self.softmax(logits_cat), labels_cat) + LQLoss(0.4, self.sentiment_weights)(self.softmax(logits_pol), labels_pol)
         return loss, logits_cat, logits_pol
